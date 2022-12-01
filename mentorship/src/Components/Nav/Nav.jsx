90985974a10f90c3831d352738189c5dbb782ba2
Boateng Oduro Kelvin <83387330+codingbagh@users.noreply.github.com>
2022-12-01 01:49:10 +0000

    .

---
+++ mentorship/src/Components/Nav/Nav.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "../../style/nav.css";
+import facebookIcon from "../Images/facebook.png";
+import search from "../Images/search.png";
+import watchIcon from "../Images/facebookWatch.png"
+import  gaming from "../Images/messenger.png"
+import  menu from "../Images/hamburger.png"
+import bell from "../Images/bell.png"
+import man from "../Images/man.png"
+import menu2 from "../Images/dots-menu.png"
+import home from "../Images/home.png"
+function Nav(props) {
+  return (
+    <div id="navContainer">
+
+        <div>
+               <img src={facebookIcon} className="fbIcon" alt="" />
+      <img src={search} className="searchIcon" alt="" />
+        </div>
+        <div className="middle">
+        <img src={home} className="fbIcon2" alt="" />
+
+        <img src={watchIcon} className="fbIcon2" alt="" />
+
+        <img src={gaming} className="fbIcon2" alt="" />
+        <img src={menu} className="fbIcon2" alt="" />
+        </div>
+
+        <div className="middle2">
+        <img src={menu2} className="fbIcon3" alt="" />
+
+        <img src={gaming} className="fbIcon3" alt="" />
+
+        <img src={bell} className="fbIcon3" alt="" />
+        <img src={man} className="fbIcon3" alt="" />
+        </div>
+   
+    </div>
+  );
+}
+
+export default Nav;
---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+const App=()=> {
+
+  return (
+    <> 
+      <div className="App">
+     <h1>hksksksksk</h1>
+    </div>
+    <div>
+      <h2>hello world</h2>
+    </div>
+    </>
+   
+  );
+}
+
+export default App;
